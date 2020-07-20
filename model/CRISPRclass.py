44f06ee7bf978d269c6ae6b225c299faa575b61b
Misha Klein <klein.misha@gmail.com>
2020-07-20 15:42:24 +0200

    added PAM_direction: enter '3 to 5' or '5 to 3' to signify if PAM is on 5' side or on 3' side.

--- model/CRISPRclass.py
+++ model/CRISPRclass.py
@@ -1,6 +1,8 @@
 class CRISPR():
-    def __init__(self,guide_length,PAM_length, PAM_sequence):
+    def __init__(self,guide_length,PAM_length, PAM_sequence,
+                 PAM_direction='5 to 3' ):
         self.guide_length = guide_length
         self.PAM_length = PAM_length
         self.target_length = self.guide_length + self.PAM_length
         self.PAM = PAM_sequence
+        self.PAM_direction = PAM_direction
