d482bd5b90769d83f3c75b7641281ef92630472f
Hidde Offerhaus <hiddeofferhaus@hpc05.hpc>
2022-01-17 14:58:32 +0100

    second results set from cluster

--- run/testrun.sh
+++ run/testrun.sh
@@ -2,8 +2,8 @@
 #PBS -N CRISPR_kinetic_model
 #PBS -l nodes=1:ppn=20
 #PBS -M H.S.Offerhaus@tudelft.nl
-#PBS -o /home/hiddeofferhaus/depkengit/CRISPR_kinetic_model/results/hpc_test/stdout.txt
-#PBS -e /home/hiddeofferhaus/depkengit/CRISPR_kinetic_model/results/hpc_test/stderr.txt
+#PBS -o /home/hiddeofferhaus/depkengit/CRISPR_kinetic_model/results/hpc_test2/stdout.txt
+#PBS -e /home/hiddeofferhaus/depkengit/CRISPR_kinetic_model/results/hpc_test2/stderr.txt
 
 # Start job
 echo "starting job..."
@@ -14,4 +14,4 @@ source activate crispr_kin_model_env
 export PYTHONPATH="${project_folder}/model"
 
 # Run python script
-python "${project_folder}/run/fit_data.py" "${project_folder}/results/hpc_test/new_log.txt"
+python "${project_folder}/run/fit_data.py" "${project_folder}/results/hpc_test2/new_log.txt"
