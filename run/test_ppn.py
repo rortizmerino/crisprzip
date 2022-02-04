f302af5c92c283e6c12975dfc5d13b7a23cfd088
Hidde Offerhaus <hso@live.nl>
2022-02-04 18:04:31 +0100

    Finished ppn test

--- run/test_ppn.py
+++ run/test_ppn.py
@@ -19,20 +19,30 @@ def find_prime_numbers(upto):
     return prime_list[-1]
 
 
-def main(job_no, out_path='results/', array_id=1):
-    job_no = int(job_no)
+def main(proc_no, out_path='results/', array_id=1):
+    proc_no = int(proc_no)
 
+    # find first x prime numbers
+    x = 5000
+
+    # 120 runs = 24 * 5
     start = time.time()
-    for _ in range(job_no):
-        find_prime_numbers(1000)
-    ref_time = time.time() - start
+    for _ in range(5):
+        _ = find_prime_numbers(x)
+    ref_time = 24 * (time.time() - start)
+    print('Single process:     %.2f s' % ref_time)
 
+    # 120 parallellized runs
     start = time.time()
     _ = (
-        Parallel(n_jobs=job_no)
-        (delayed(find_prime_numbers)(i) for i in job_no*[1000])
+        Parallel(n_jobs=proc_no)
+        (delayed(find_prime_numbers)(i) for i in 120*[x])
     )
     quicker_time = time.time() - start
+
+    print('Parallel processes: %.2f s' % quicker_time)
+    print('Extra time:         %.2f s' % (quicker_time - ref_time / proc_no))
+    print()
     print('Speedup: %.2f' % (ref_time/quicker_time))
 
 
