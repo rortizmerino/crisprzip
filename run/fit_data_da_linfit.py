25e9d82978440a76e25b2d12565d955a908d3d28
hiddeofferhaus <H.S.Offerhaus@tudelft.nl>
2022-03-14 17:14:34 +0100

    Script for linear fit

---
+++ run/fit_data_da_linfit.py
@@ -0,0 +1,94 @@
+import sys
+import os
+
+# on Hidde's private laptop
+if os.path.exists('C:/Users/HP/depkengit/CRISPR_kinetic_model'):
+    sys.path.append('C:/Users/HP/depkengit/CRISPR_kinetic_model')
+
+# on Hidde's TU laptop
+if os.path.exists('C:/Users/hiddeofferhaus/CRISPR_kinetic_model'):
+    sys.path.append('C:/Users/hiddeofferhaus/CRISPR_kinetic_model')
+
+import numpy as np
+
+from model.training_set import read_dataset, TrainingSet
+from model.fit_optimizer import track_dual_annealing
+
+
+def get_root_dir(script_path):
+    root_dir = os.path.abspath(
+        os.path.join(
+            os.path.dirname(os.path.abspath(script_path)),  # parent dir (=/run)
+            os.pardir  # /.. (up a directory)
+        )
+    )
+    return root_dir
+
+
+def main(local_search, script_path='./fit_data_da.py',
+         out_path='results/local_test/', array_id=1):
+
+    # FIT SETTINGS
+    dual_annealing_kwargs = {
+        'no_local_search': not bool(local_search),
+        'maxiter': 2500,
+        'maxfun': 250000,
+        # 'maxfun': 25,
+    }
+
+    # initial vector and bounds
+    initial_param_vector = np.ones(shape=(44,))
+    param_lower_bounds = np.array(21 * [-10] + 20 * [0] + 3 * [-6])
+    param_upper_bounds = np.array(41 * [20] + 3 * [6])
+    param_bounds = np.stack([param_lower_bounds,
+                             param_upper_bounds], axis=1)
+
+    # collecting arguments
+    root_dir = get_root_dir(script_path)
+    out_dir = os.path.abspath(out_path)
+
+    # preparing champ and nucleaseq data (ORIGINAL DATASET)
+    experiments = ['NucleaSeq', 'Champ']
+    datasets = []
+    for exp in experiments:
+        path = os.path.join(root_dir, f'data/SpCas9/{exp}2020/orig_data.csv')
+        datasets += [read_dataset(path)]
+
+    # make training set
+    training_set = TrainingSet(datasets=datasets,
+                               experiment_names=experiments,
+                               experiment_weights=[1, 1],
+                               weigh_error=True, rel_error=False,
+                               weigh_multiplicity=True,
+                               normalize_weights=False)
+
+    get_lin_msd = lambda param_vec: training_set.get_cost(param_vec,
+                                                          multiprocessing=True,
+                                                          log_msd=False)
+
+    # run the optimization
+    _ = track_dual_annealing(
+        func=get_lin_msd,
+        x0=initial_param_vector,
+        bounds=param_bounds,
+        out_path=out_dir,
+        **dual_annealing_kwargs
+    )
+
+
+if __name__ == "__main__":
+
+    # (cluster) keyword arguments: script_path, array_id and out_path
+    kwargs = {'script_path': sys.argv[0]}
+    for arg in sys.argv[1:]:
+        if '=' in arg:
+            key, val = arg.split('=')
+            if key == 'array_id':
+                kwargs[key] = int(val)
+            else:
+                kwargs[key] = val
+
+    # arguments: anything needed for this script
+    args = [arg for arg in sys.argv[1:] if not ('=' in arg)]
+
+    main(*args, **kwargs)
