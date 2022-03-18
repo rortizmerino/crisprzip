37b92d24459879dcd8bc7f43d7c105d2daa63f36
Hidde Offerhaus <H.S.Offerhaus@tudelft.nl>
2022-03-18 15:36:25 +0100

    Improved cluster interaction (downloads to project drive)

--- run/cluster/collect_results.sh
+++ run/cluster/collect_results.sh
@@ -2,7 +2,7 @@
 
 # input
 my_dir="$(dirname "$0")"
-source "${my_dir}/user.config"  # contains netid, email, root_dir
+source "${my_dir}/user.config"  # contains netid, email, root_dirs
 remote="${netid}@hpc05.tudelft.net"
 
 # check if results dir empty
@@ -12,29 +12,38 @@ if [[ -z $new_job_dirs ]]; then
   exit 0
 fi
 
-# collecting results
-printf "Collecting results...\n"
-rsync -rv --min-size=1 --remove-source-files "${remote}:${remote_root_dir}/results/" "${local_root_dir}/results"
-# min-size prevents empty (typically, stdout) files from being copied
-
-# copy results to project drive - FINISH!
-#echo
-#read -p "Copy results to the project drive? (y/n): " -n 1 -r
-#if [[ $REPLY =~ ^[Yy]$ ]]; then
-#    rsync -rv "${local_root_dir}/results"
-#    echo 'Copied to project drive'
-#fi
-
-# after rsync, we remove all results to keep the cluster clean
-echo
-ssh "$remote" "find ${remote_root_dir}/results/ -type f -size 0 -print"
-ssh "$remote" "find ${remote_root_dir}/results/ -type d -mindepth 1 -print"
-# prompt for confirmation
-read -p "Remove the above empty files and folders from the results directory? (y/n): " -n 1 -r
+# collecting the results
+read -p "Copy results directly to the project drive? (y/n): "  -r
 if [[ $REPLY =~ ^[Yy]$ ]]; then
-    ssh "$remote" "find ${remote_root_dir}/results/ -type f -size 0 -delete";
-    ssh "$remote" "find ${remote_root_dir}/results/ -type d -mindepth 1 -empty -delete";
-    echo 'Removed from cluster'
+
+  # download to project drive
+  for dir in $(ssh "$remote" "ls -d ${remote_root_dir}/results/*"); do
+    sftp -r "${remote}:${dir} ${project_drive_dir}/hpc_results";
+  done;
+  echo "Copied to project drive";
+
+else
+  # get local path
+  read -p "Copy to local path (default: ${local_root_dir}/results): " -r local_path;
+  if [[ $local_path = "" ]]; then local_path="${local_root_dir}/results"; fi;
+  if [ ! -d "$local_path" ]; then mkdir -p "$local_path"; fi;
+
+  # download to local path
+  for dir in $(ssh "$remote" "ls -d ${remote_root_dir}/results/*"); do
+    sftp -r "${remote}:${dir} ${local_path}";
+  done;
+  echo "Copied to ${local_path}";
+fi
+
+# after downloading, we remove all results to keep the cluster clean
+echo
+ssh "$remote" "find ${remote_root_dir}/results/ -mindepth 1 -print" | sort
+
+# prompt for confirmation
+read -p "Remove the above files and folders from the results directory? (y/n): " -r
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+    ssh "$remote" "find ${remote_root_dir}/results/ -mindepth 1 -delete";
+    echo "Removed from cluster";
 fi
 
 # remove everything from the .temp folder
