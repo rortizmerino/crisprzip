b9952699f2409fc0ed69d08e9f9101604ea84dfb
Hidde Offerhaus <hso@live.nl>
2023-01-16 11:46:00 +0100

    update for new modules

--- model/__init__.py
+++ model/__init__.py
@@ -1,5 +1,8 @@
-import model.fit_analysis
+import model.analysis
+import model.data
 import model.experiment_simulations
-import model.fit_optimizer
-import model.hybridization_kinetics
-import model.training_set
+import model.fit_analysis
+import model.fitting
+import model.fitting
+import hybridization_kinetics
+import parameter_vector
