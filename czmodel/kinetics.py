8f3894687257de3b961233ad512b962645f4eb57
Hidde Offerhaus <H.S.Offerhaus@tudelft.nl>
2023-12-05 11:43:56 +0100

    Normalize output for var init

--- czmodel/kinetics.py
+++ czmodel/kinetics.py
@@ -551,7 +551,10 @@ class SearcherTargetComplex(Searcher):
         landscape_occupancy[:, unsafe] = (landscape_occupancy[:, unsafe] *
                                           float('nan'))
 
-        return np.squeeze(landscape_occupancy.T)
+        if vary_init:
+            return np.squeeze(landscape_occupancy)
+        else:
+            return np.squeeze(landscape_occupancy.T)
 
     def get_cleaved_fraction(self, time: Union[float, np.ndarray],
                              on_rate: float) -> np.ndarray:
