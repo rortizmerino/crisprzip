132f42e3ed2fb8642214fd6ecd8c729e701b53dd
Hidde Offerhaus <H.S.Offerhaus@tudelft.nl>
2024-04-08 15:51:59 +0200

    Replacing environment by requirements

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name='crisprzip-model',
+    version='0.0.0',
+    packages=['czmodel'],
+    install_requires=['numpy==1.20.3', 'scipy==1.7.1', 'joblib==1.1.0']
+)
