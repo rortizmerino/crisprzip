e91faca464f18385aa3291bb80ebbe2c81a20153
Hidde Offerhaus <H.S.Offerhaus@tudelft.nl>
2025-01-07 16:38:57 +0100

    Updated copyright (page footer)

--- docs/conf.py
+++ docs/conf.py
@@ -7,7 +7,7 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'CRISPRzip'
-copyright = '2024, HS Offerhaus'
+copyright = '2024, HS Offerhaus, MIT license'
 author = 'HS Offerhaus'
 release = '1.0'
 
